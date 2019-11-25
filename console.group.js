7e94d399879e3270a94447ecf70f9d8bf8c8fd58
Nadim Saker <nadimsaker@gmail.com>
2019-11-25 10:15:46 +0600

    console.group added

---
+++ console.group.js
@@ -0,0 +1,11 @@
+console.group("Parent");
+console.log("Child1");
+console.log("Child2");
+console.log("Child3");
+console.groupEnd("End Parent");
+
+console.group("Parent Next");
+console.log("Child4");
+console.log("Child5");
+console.log("Child6");
+console.groupEnd("End Parent");
