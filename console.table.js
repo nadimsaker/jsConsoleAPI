32eda16ef55f52447cefa68cf7d0ce4215da7401
Nadim Saker <nadimsaker@gmail.com>
2019-11-25 10:32:09 +0600

    Add console.table

---
+++ console.table.js
@@ -0,0 +1,24 @@
+const users = [
+  {
+    Name: "Nadim",
+    DateOfBirth: "Dec 8 1977",
+    Address: "Dhaka"
+  },
+  {
+    Name: "Sakib",
+    DateOfBirth: "Dec 8 1990",
+    Address: "Dhaka"
+  },
+  {
+    Name: "Sakib",
+    DateOfBirth: "Dec 8 1990",
+    Address: "Dhaka"
+  },
+  {
+    Name: "Sakib",
+    DateOfBirth: "Dec 8 1990",
+    Address: "Dhaka"
+  }
+];
+
+console.table(users);
