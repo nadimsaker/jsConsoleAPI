3c86945d01955a1aab0ddb9905e77278bb1f6c77
Nadim Saker <nadimsaker@gmail.com>
2019-11-25 11:43:01 +0600

    updated

--- console.time.js
+++ console.time.js
@@ -1,12 +1,12 @@
 const startTime = Date.now();
-for (let i = 1; i < 1000; i++) {
+for (let i = 1; i < 100; i++) {
   console.log(i * i);
 }
 const timeDiff = Date.now() - startTime;
 console.log(timeDiff);
 
-console.time("Time start");
-for (let i = 1; i < 1000; i++) {
+console.time("Time");
+for (let i = 1; i < 100; i++) {
   console.log(i * i);
 }
-console.timeEnd("Time End:");
+console.timeEnd("Time");
